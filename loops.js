d30971183e9cdad1742f3147218f513cae508747
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-09 06:57:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ let strangeArray = []
 
 function forLoop(strangeArray) {
   for (let i = 0; i < 25; i++) {
-    if (i === 0) {
+    if (i === 1) {
       strangeArray.push(`I am ${i} strange loop.`)
     } else {
       strangeArray.push(`I am ${i} strange loops.`)
