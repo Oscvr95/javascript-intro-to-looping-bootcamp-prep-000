24b160587eff0fcf3be09088c11c9fd5394733be
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-09 06:24:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ let strangeArray = []
 
 function addStringWithForLoop(arr) {
   for (let i = 0; i < 25; i++) {
-    if (i === 1) {
+    if (i === 0) {
       arr.push("I am  strange loop.")
     } else {
       arr.push("I am ${i} strange loops")
